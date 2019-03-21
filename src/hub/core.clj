6f18d406226c3992b44e48c4a6ee614ae054c219
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2019-03-21 11:06:47 -0400

    remove unused code in core

--- src/hub/core.clj
+++ src/hub/core.clj
@@ -7,7 +7,3 @@
   "Bootstrap system as a web server and expose endpoints for submission of data."
   [& args]
   (server/run))
-
-  ;(let [m1 {:topic :log :content {:key1 "val1"}}]
-  ;(publish (:topic m1) (:content m1))
-  ;(println "Complete!")))
